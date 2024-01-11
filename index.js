74317659ac9c15e03bf43438b3b475946bc316a0
Voltzy <voltzy240@gmail.com>
2024-01-11 11:21:54 +1100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = '';
 var password = '';
 var shared_secret = '';
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730, 440, 570, 438100, 1172470, 1905180, 2381340, 2061630, 2388360, 2471900, 952950, 2362150, 2147580, 2450220, 2305790, 2426500, 1951950, 2025280, 1059420, 1557760, 2335720, 2442880, 2396620, 2487640, 2266370, 2284810, 2272090, 2358610, 2439100, 2308220];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
